a42c709bc7278ffb5a7404264cb9c2be6a6d2859
Dymytriy Zyunkin <dzyunkin@gmail.com>
2025-01-17 15:21:27 -0500

    fixes

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # KafkaGuardian
 ML powered tool that detects anomalies in Kafka producer behavior
+
+To run the repo:
+```
+cd KafkaGuardian
+python -m venv .venv
+.venv\Scripts\activate
+pip install -r requirements.txt
+python main.py
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from kafka import KafkaProducer
-from objects import User, UserFactory
+from objects.UserFactory import UserFactory
+from objects.User import User
 from utils.simulate_users import run_simulation
 from typing import List
 
@@ -8,17 +9,16 @@ def main():
     producer = KafkaProducer(bootstrap_servers="localhost:9092")
     topic = "demo-messages"
 
-    factory = UserFactory(producer, topic)
-    user_configs = [
+    factory: UserFactory = UserFactory(producer, topic)
+    user_configs: List[tuple] = [
         (1.0, 0.5),  # User 1
         (1.5, 0.7),  # User 2
     ]
-    users = factory.create_multiple_users(user_configs)
+    users: List[User] = factory.create_multiple_users(user_configs)
 
     # Run simulation for 5 seconds
     run_simulation(users, duration=5)
 
 
-# Example usage:
 if __name__ == "__main__":
     main()
